6cff11f6307fc413d056df69228110d1a7d39a8e
vasikrish777 <42526706+vasikrish777@users.noreply.github.com>
2019-08-25 17:16:24 +0530

    Add files via upload

---
+++ vasi.py
@@ -0,0 +1,4 @@
+passwd = input("Enter the password: ") 
+while passwd != "book":
+     print("Wrong password: ") 
+     passwd = input("Enter your password: ") print("You have successfully logged in")
